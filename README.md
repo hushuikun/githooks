a363c4a1022b3abdf6cc7044126da708b3e21f71
hushuikun <ken.hu@kilimall.com>
2025-02-25 11:11:37 +0800

    feat(hooks): 添加pre-commit和commit-msg钩子

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # githooks
+
+## 安装
+
+```bash
+curl -sSL https://raw.githubusercontent.com/hushuikun/githooks/refs/heads/test/script.sh | bash
+```
---
+++ script.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# 检测当前目录是否存在.git/hooks
+if [ -d ".git/hooks" ]; then
+    echo "当前目录存在.git/hooks"
+    
+    # 下载钩子
+    curl -o .git/hooks/pre-commit https://raw.githubusercontent.com/hushuikun/githooks/refs/heads/test/hooks/pre-commit
+    curl -o .git/hooks/commit-msg https://raw.githubusercontent.com/hushuikun/githooks/refs/heads/test/hooks/commit-msg
+
+    # 添加执行权限
+    chmod +x .git/hooks/pre-commit
+    chmod +x .git/hooks/commit-msg
+
+    # 输出添加钩子完成的信息
+    echo "添加钩子完成"
+else
+    echo "当前目录不存在.git/hooks"
+fi
